711c125f4e2688eb3e32ba18c215cc77f59c6243
Yann Mulonda <yann.mulonda@icfnext.com>
2021-05-23 23:15:25 -0500

    added local Host ip address

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import routes from './source/routes/route.js';
 // constant variables 
 const app = express();
 const PORT = 3000;
+const HOST = '0.0.0.0';
 
 // body parser setup for Express v4.16.0 and higher
 app.use(express.json());
@@ -18,6 +19,6 @@ app.use(express.urlencoded(
 routes(app);
 
 // Setting the server to listen at port 3000
-app.listen(PORT, function () {
-  console.log(`Server started on http://localhost:${PORT}`);
+app.listen(PORT, HOST, function () {
+  console.log(`Server started on http://${HOST}:${PORT}`);
 });
